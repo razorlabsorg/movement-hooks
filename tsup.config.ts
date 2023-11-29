67b0172a4f02b015fb5eb7742b248aa6eb881f92
Nate <89227631+natedotv2@users.noreply.github.com>
2023-11-29 04:34:21 +0100

    Added connectors package

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: {
+        core: 'core/src/index.ts',
         index: 'src/index.ts',
+        'connectors/petra': 'core/src/connectors/petra.ts',
+        'connectors/pontem': 'core/src/connectors/pontem.ts',
+        'connectors/razor': 'core/src/connectors/razor.ts',
     },
     format: ['esm', 'cjs'],
     dts: true,
